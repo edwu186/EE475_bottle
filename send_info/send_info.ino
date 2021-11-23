255a7482523afbe2a30599c2ff121d460324033a
Peter Gunarso <gunarp@cs.washington.edu>
2021-11-23 10:46:36 -0800

    update paket lengths

--- send_info/send_info.ino
+++ send_info/send_info.ino
@@ -7,27 +7,38 @@ RF24 radio(9,10);
 
 BTLE btle(&radio);
 
-int i;
+bool i;
 
 void setup() {
 
   Serial.begin(9600);
   while (!Serial) { }
   Serial.println("BTLE advertisement sender");
-  i = 0;
+  i = true;
   btle.begin("PPal");
 
 }
 
 void loop() {
-  Serial.print("Advertising Packet ");
-  Serial.print(i);
-
+  // Serial.print("Advertising Packet ");
+  // Serial.print(i);
   bool succ = btle.advertise((void *) REGISTER_P, (uint8_t) PACKET_LEN);
-  Serial.println(succ);
+  // bool succ = btle.advertise((void *) BEACON_P, (uint8_t) PACKET_LEN);
+  // bool succ = btle.advertise((void *) CLOSE_P, (uint8_t) PACKET_LEN);
+  // bool succ = btle.advertise((void *) OPEN_P, (uint8_t) PACKET_LEN);
+  // if (i) {
+  //   bool succ = btle.advertise((void *) REGISTER_P, (uint8_t) PACKET_LEN);
+  //   Serial.print("Register Packet  ");
+  //   Serial.println(succ);
+  //   i = false;
+  // } else {
+  //   bool succ = btle.advertise((void *) BEACON_P, (uint8_t) PACKET_LEN);
+  //   Serial.print("Beacon Packet  ");
+  //   Serial.println(succ);
+  //   i = true;
+  // }
   btle.hopChannel();
   delay(50);
-  i++;
 
   // delay(100);
 }
